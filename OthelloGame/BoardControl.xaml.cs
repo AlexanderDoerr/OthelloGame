841872f4be19772cc9fe54ed1636e923d12aaa89
Collin Kite <me@collinkite.com>
2023-02-21 15:21:35 -0700

    Added Connecting Game Array to Frontend Board and Updating it

--- OthelloGame/BoardControl.xaml.cs
+++ OthelloGame/BoardControl.xaml.cs
@@ -26,9 +26,35 @@ namespace OthelloGame
         {
             InitializeComponent();
             game = new Game();
+            updateGrid();
         }
 
-
+        private void updateGrid()
+        {
+            //get the board from the game and update the grid
+            Colors[,] board = game.board;
+            //get the grid from the xaml
+            Grid grid = (Grid)FindName("grid");
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    if (board[row, col] == Colors.black)
+                    {
+                        //black
+                        //get the image elements from the grid
+                        Image img = (Image)grid.Children.Cast<UIElement>().Last(x => Grid.GetRow(x) == row && Grid.GetColumn(x) == col);
+                        img.Source = new BitmapImage(new Uri("assets/BlackPiece_lg.png", UriKind.Relative));
+                    }
+                    else if (board[row, col] == Colors.white)
+                    {
+                        //white
+                        Image img = (Image)grid.Children.Cast<UIElement>().Last(x => Grid.GetRow(x) == row && Grid.GetColumn(x) == col);
+                        img.Source = new BitmapImage(new Uri("assets/WhitePiece_lg.png", UriKind.Relative));
+                    }
+                }
+            }
+        }
 
         private void Image_OnMouseMouseDown(object sender, RoutedEventArgs routedEventArgs)
         {
@@ -37,7 +63,17 @@ namespace OthelloGame
             int row = Grid.GetRow(btn);
             int col = Grid.GetColumn(btn);
             Image img = (Image)grid.Children.Cast<UIElement>().Last(x => Grid.GetRow(x) == row && Grid.GetColumn(x) == col);
-            img.Source = new BitmapImage(new Uri("assets/BlackPiece_lg.png", UriKind.Relative));
+            //add piece to game class
+            if (game.isBlackTurn)
+            {
+                img.Source = new BitmapImage(new Uri("assets/BlackPiece_lg.png", UriKind.Relative));
+                
+            }
+            else
+            {
+                img.Source = new BitmapImage(new Uri("assets/WhitePiece_lg.png", UriKind.Relative));
+            }
+            game.isBlackTurn = !game.isBlackTurn;
         }
     }
 }
