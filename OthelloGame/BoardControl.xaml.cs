1e8fcef31a02decc6f97a77160373b95de52e3a6
AMadmanWithABox <carter.gamble014@gmail.com>
2023-02-21 14:55:56 -0700

    Made buttons work better and added more events

--- OthelloGame/BoardControl.xaml.cs
+++ OthelloGame/BoardControl.xaml.cs
@@ -33,6 +33,25 @@ namespace OthelloGame
             int col = Grid.GetColumn(btn);
             Image img = (Image)grid.Children.Cast<UIElement>().Last(x => Grid.GetRow(x) == row && Grid.GetColumn(x) == col);
             img.Source = new BitmapImage(new Uri("assets/BlackPiece_lg.png", UriKind.Relative));
+            btn.IsEnabled = false;
+        }
+
+        private void Button_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            // We will add code here to check if the move is valid and then change the color of the button
+            // to indicate that it is a valid move.
+            // for now it will just change the color of the button to gray
+            Button btn = (Button)sender;
+            if (btn.IsEnabled)
+            {
+                btn.Background = new SolidColorBrush(Colors.Gray);
+            }
+        }
+
+        private void Button_OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Background = new SolidColorBrush(Colors.Transparent);
         }
     }
 
